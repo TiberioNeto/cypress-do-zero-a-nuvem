544a3c7ba6ea9a39a6a57be1f2d602e8ac7cc8c2
Tibério Neto <athanetto@gmail.com>
2025-02-06 16:10:46 -0300

    03 - Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,7 +6,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('Verifica o título da aplicação', () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT")
   })
-  it.only("Preenche os campos obrigatórios e envia o formulário", () => {
+  it("Preenche os campos obrigatórios e envia o formulário", () => {
     const longText = Cypress._.repeat("Filha, papai te ama, ", 20)
     cy.get('#firstName').type("Tibério")
     cy.get('#lastName').type("Neto")
@@ -15,5 +15,14 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.contains("button", "Enviar").click()
 
     cy.get(".success").should("be.visible")
+  })
+  it.only("Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
+    cy.get('#firstName').type("Tibério")
+    cy.get('#lastName').type("Neto")
+    cy.get('#email').type("athanetto@gmail,com")
+    cy.get('#open-text-area').type("teste")
+    cy.contains("button", "Enviar").click()
+
+    cy.get(".error").should("be.visible")
   })
 })

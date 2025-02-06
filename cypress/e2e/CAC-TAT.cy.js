d3456eb76ce461d2642b423c5f389ed00cc9a2b1
Tibério Neto <athanetto@gmail.com>
2025-02-06 15:44:14 -0300

    02 - Preenche os campos obrigatórios e envia o formulário

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,5 +6,14 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('Verifica o título da aplicação', () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT")
   })
+  it.only("Preenche os campos obrigatórios e envia o formulário", () => {
+    const longText = Cypress._.repeat("Filha, papai te ama, ", 20)
+    cy.get('#firstName').type("Tibério")
+    cy.get('#lastName').type("Neto")
+    cy.get('#email').type("athanetto@gmail.com")
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.contains("button", "Enviar").click()
 
+    cy.get(".success").should("be.visible")
+  })
 })

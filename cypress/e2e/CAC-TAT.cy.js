391f22fc95600df7b584d39df68b87c9caff34a0
Tibério Neto <athanetto@gmail.com>
2025-02-06 23:37:57 -0300

    18 - verifica que a política de privacidade abre em outra aba sem a necessidade de um clique

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -123,12 +123,17 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         expect(input[0].files[0].name).to.equal("example.json")
       })
   })
-  it.only("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", () => {
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", () => {
     cy.fixture('example.json').as('sampleFile')
     cy.get('#file-upload')
       .selectFile('@sampleFile')
       .should(input => {
         expect(input[0].files[0].name).to.equal("example.json")
       })
+  })
+  it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .should('have.attr','href', 'privacy.html')
+    .and('have.attr', 'target', '_blank')
   })
 })

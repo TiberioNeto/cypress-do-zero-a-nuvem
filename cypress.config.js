56aeecf2e8e223205d75480cc3322046f7a6c830
Tibério Neto <athanetto@gmail.com>
2025-02-07 18:57:24 -0300

    configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "hzqt8w",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {}
 })
 
-//npm run cy:open

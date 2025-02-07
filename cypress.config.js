4778bf0f9541c8ca0c5984952a37314d3152b581
Tibério Neto <athanetto@gmail.com>
2025-02-07 17:34:06 -0300

    atualizando configurações

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
-  video: true
-})
+  e2e: {}
 
 //npm run cy:open
